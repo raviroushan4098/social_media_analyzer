3028bcfaeb58aa2dac3220ce7f626a52c27ba956
raviroushan4098 <124333586+raviroushan4098@users.noreply.github.com>
2025-04-23 11:16:56 +0530

    Update settings.py

--- social_media_analyzer/settings.py
+++ social_media_analyzer/settings.py
@@ -94,9 +94,7 @@ DATABASES = {
     }
 }
 
-DATABASES = {
-    'default': dj_database_url.config(default=config('DATABASE_URL'))
-}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
